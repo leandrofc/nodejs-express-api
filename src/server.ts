e78d645f411becdc79b649b0ac4b5f8b7d8a4be4
Leandro Fonseca <leandromkt@outlook.com>
2025-07-21 17:31:13 -0300

    configuracoes do express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express, { Request, Response } from "express";
+import express, { json, Request, Response } from "express";
 
 const app = express();
+const port = process.env.PORT;
+
+app.use(json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("hello world");
+    res.status(200).json({ player: "beckham"});
 });
 
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`Server running at port http://localhost:${port}`)
+})
