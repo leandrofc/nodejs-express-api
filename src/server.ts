eefdf2350709ea43542a1928aaa721f504414d69
Leandro Fonseca <leandromkt@outlook.com>
2025-07-21 17:35:53 -0300

    separando a server da app

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import express, { json, Request, Response } from "express";
+import createApp from "./app";
 
-const app = express();
+const app = createApp();
 const port = process.env.PORT;
-
-app.use(json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({ player: "beckham"});
-});
 
 app.listen(port, () => {
     console.log(`Server running at port http://localhost:${port}`)
